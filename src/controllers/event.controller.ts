a2e5c909a01320beac292e64f8a0e86613eeb2cb
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-08-11 15:41:01 -0300

    feat: add multer

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import EventService from "../services/event.service";
+import Event from "../entities/Event";
 
 export default class EventController {
   constructor(private eventService: EventService) {}
 
   async create(req: Request, res: Response, next: NextFunction) {
-    const eventData = req.body;
+    const eventData: Event = req.body;
 
     try {
       await this.eventService.create(eventData);
--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { EventRepositoryMongoose } from "../repositories/event.repository.mongoose";
 import EventController from "../controllers/event.controller";
 import EventService from "../services/event.service";
+import upload from "../infra/multer";
 
 export default class EventRoutes {
   public router: Router;
@@ -18,6 +19,16 @@ export default class EventRoutes {
   initRoutes() {
     this.router.post(
       "/",
+      upload.fields([
+        {
+          name: "banner",
+          maxCount: 1,
+        },
+        {
+          name: "flyers",
+          maxCount: 3,
+        },
+      ]),
       this.eventController.create.bind(this.eventController)
     );
   }
